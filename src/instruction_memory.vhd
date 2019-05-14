e1757d09a20343eea411dceb970dd58cc82ecf42
YEmreAk <yedhrab@gmail.com>
2019-05-14 21:19:58 +0300

    İnstructionlar yazıldı

--- src/instruction_memory.vhd
+++ src/instruction_memory.vhd
@@ -11,19 +11,12 @@ end Instruction_Memory_VHDL;
 
 architecture Behavioral of Instruction_Memory_VHDL is
 signal rom_addr: std_logic_vector(3 downto 0);
- -- lw $3, 0($0) -- pc=0
- -- Loop: sltiu  $1, $3, 11= pc = 2
- -- beq $1, $0, Skip = 4 --PCnext=PC_current+2+BranchAddr
- -- add $4, $4, $3 = 6
- -- addi $3, $3, 1 = 8
- -- beq $0, $0, Loop--a
- -- Skip c = 12 = 4 + 2 + br
- type ROM_type is array (0 to 15 ) of std_logic_vector(15 downto 0);
- constant rom_data: ROM_type:=(
+type ROM_type is array (0 to 15 ) of std_logic_vector(15 downto 0);
+constant rom_data: ROM_type:=(
    "1000100010000001", -- lw
    "0001011000110001", -- sub
    "0001111100100010", -- and
-   "0001001011000100" -- or,
+   "0001001011000100", -- or,
    "0001101111010000", --  add
    "0110010011101000", -- bne
    "1100010101111100", -- beq $1 $2 -4
